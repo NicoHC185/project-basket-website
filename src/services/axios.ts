96139750cba31788b61edab1ceec8eff6992b30d
NicoHC185 <nikohidalgo.castillo@gmail.com>
2024-04-11 23:33:29 -0400

    MOD: conect url to env

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = process.env.NEXT_PUBLIC_URL || "http://localhost:3000/api";
+const url = process.env.NEXT_PUBLIC_URL;
 const axiosService = axios;
 axiosService.defaults.baseURL = url;
 
