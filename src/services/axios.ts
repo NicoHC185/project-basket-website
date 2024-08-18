32ca65246b6694267b74e26ac1a6ca18e2fc3754
NicoHC185 <nikohidalgo.castillo@gmail.com>
2024-08-18 11:24:00 -0400

    MOD: change axios service url using env

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = process.env.NEXT_PUBLIC_URL;
+const url = process.env.NEXT_PUBLIC_URL ?? 'http://localhost:8000/api';
 const axiosService = axios;
 axiosService.defaults.baseURL = url;
 
