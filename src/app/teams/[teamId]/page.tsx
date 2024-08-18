15f0fdb042cffbc4f517c31a6f1fbf8d5a4fb557
NicoHC185 <nikohidalgo.castillo@gmail.com>
2024-08-18 11:24:46 -0400

    MOD: change fetch fn request

--- src/app/teams/[teamId]/page.tsx
+++ src/app/teams/[teamId]/page.tsx
@@ -1,79 +1,93 @@
-'use client'
+"use client";
 
-import { Skeleton, Stack, Typography } from "@mui/material"
-import MainCard from "components/cards/MainCard"
-import { IInfoPlayer, IInfoTeam } from "interfaces"
-import moment from "moment"
-import { useParams } from "next/navigation"
-import { useEffect, useState } from "react"
-import { axiosService } from "services/axios"
-import TableRoster from "./TableRoster"
-import SubCard from "components/cards/SubCard"
+import { Skeleton, Stack, Typography } from "@mui/material";
+import MainCard from "components/cards/MainCard";
+import { IInfoPlayer, IInfoTeam } from "interfaces";
+import moment from "moment";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import TableRoster from "./TableRoster";
+import SubCard from "components/cards/SubCard";
+
+const url = process.env.NEXT_PUBLIC_URL ?? "http://localhost:8000/api";
 
 const Team = () => {
-  const { teamId } = useParams()
-  const [infoTeam, setInfoTeam] = useState<IInfoTeam | null>(null)
-  const [roster, setRoster] = useState<IInfoPlayer[]>([])
-  const [load, setLoad] = useState(true)
+  const { teamId } = useParams();
+  const [infoTeam, setInfoTeam] = useState<IInfoTeam | null>(null);
+  const [roster, setRoster] = useState<IInfoPlayer[]>([]);
+  const [load, setLoad] = useState(true);
 
   const bodyPost = {
     codeTeam: String(teamId).toUpperCase(),
-    year: moment().format('YYYY')
-  }
+    year: moment().format("YYYY"),
+  };
 
   useEffect(() => {
-    Promise.all([getInfoTeam(), getRoster()]).then(res => {
-      const infoTeam: IInfoTeam = res[0]
-      const roster: IInfoPlayer[] = res[1]
-      setInfoTeam(infoTeam)
-      setRoster(roster)
-      setLoad(false)
-    })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    Promise.all([getInfoTeam(), getRoster()]);
+  }, []);
 
-  const getInfoTeam = () => {
+  const getRoster = async () => {
+    const { response } = await fetchRoster();
+    setRoster(response);
+    setLoad(false);
+  };
 
-    return new Promise<IInfoTeam>((resolve: Function, reject) => {
-      axiosService.post('team', bodyPost)
-        .then(({ data }: { data: IInfoTeam }) => {
-          resolve(data)
-        }).catch(err => {
-          reject(err.message)
-        })
-    })
-  }
+  const getInfoTeam = async () => {
+    const { response } = await fetchInfoTeam();
+    setInfoTeam(response);
+    setLoad(false);
+  };
 
-  const getRoster = () => {
+  const fetchInfoTeam = () => {
+    return new Promise<{ response: IInfoTeam }>(
+      async (resolve: Function, reject) => {
+        try {
+          const res = await fetch(`${url}/team`, {
+            method: "POST",
+            body: JSON.stringify(bodyPost),
+          }).then((res) => res.json());
+          resolve(res);
+        } catch (err) {
+          reject(err);
+        }
+      }
+    );
+  };
 
-    return new Promise<IInfoPlayer[]>((resolve: Function, reject) => {
-      axiosService.post('team/roster', bodyPost)
-        .then(({ data }: { data: IInfoTeam }) => {
-          resolve(data)
-        }).catch(err => {
-          reject(err.message)
-        })
-    })
-  }
+  const fetchRoster = () => {
+    return new Promise<{ response: IInfoPlayer[] }>(
+      async (resolve: Function, reject) => {
+        try {
+          const res = await fetch(`${url}/team/roster`, {
+            method: "POST",
+            body: JSON.stringify(bodyPost),
+          }).then((res) => res.json());
+          resolve(res);
+        } catch (err) {
+          reject(err);
+        }
+      }
+    );
+  };
 
-  return (
+  return load ? (
+    <Skeleton variant="rounded" width={"100%"} height={"60vh"} />
+  ) : (
+    <MainCard title={infoTeam?.name}>
+      <SubCard title="Info team">
+        <Stack>
+          <Typography>Record: {infoTeam?.record}</Typography>
+          <Typography>Coach: {infoTeam?.coach}</Typography>
+          <Typography>Executive: {infoTeam?.executive}</Typography>
+        </Stack>
+      </SubCard>
 
-    load ? <Skeleton variant="rounded" width={'100%'} height={'60vh'} /> :
-      <MainCard title={infoTeam?.name}>
-        <SubCard title="Info team">
-          <Stack>
-            <Typography>Record: {infoTeam?.record}</Typography>
-            <Typography>Coach: {infoTeam?.coach}</Typography>
-            <Typography>Executive: {infoTeam?.executive}</Typography>
-          </Stack>
-        </SubCard>
+      <Typography variant="h3" sx={{ my: 3 }}>
+        Roster
+      </Typography>
+      <TableRoster data={roster}></TableRoster>
+    </MainCard>
+  );
+};
 
-        <Typography variant="h3" sx={{ my: 3 }}>Roster</Typography>
-        <TableRoster data={roster}></TableRoster>
-
-      </MainCard >
-
-  )
-}
-
-export default Team
+export default Team;
