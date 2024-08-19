7c53891529cf91a97a73d05584386cb13e3ab7b6
NicoHC185 <nikohidalgo.castillo@gmail.com>
2024-08-18 23:17:55 -0400

    MOD: append redirect to teams page in root view

--- next.config.js
+++ next.config.js
@@ -1,21 +1,3 @@
-// /** @type {import('next').NextConfig} */
-
-// const withTM = require("next-transpile-modules")([
-//   "@mui/material",
-//   "@mui/system",
-//   "@mui/icons-material", // If @mui/icons-material is being used
-// ]);
-
-// module.exports = withTM({
-//   webpack: (config) => {
-//     config.resolve.alias = {
-//       ...config.resolve.alias,
-//       "@mui/styled-engine": "@mui/styled-engine-sc",
-//     };
-//     return config;
-//   },
-// });
-
 const nextConfig = {
   reactStrictMode: false,
   experimental: {
@@ -26,6 +8,15 @@ const nextConfig = {
   },
   compiler: {
     styledComponents: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/teams",
+        permanent: true,
+      },
+    ];
   },
 };
 
