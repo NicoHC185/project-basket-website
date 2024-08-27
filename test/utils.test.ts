703ee695fedc42e80f3e621c8599becc2cae2622
NicoHC185 <nikohidalgo.castillo@gmail.com>
2024-08-27 18:24:03 -0400

    ADD: append test config using jest and babel

--- test/utils.test.ts
+++
@@ -1,9 +0,0 @@
-import { findLogo } from "utils";
-
-describe("findLogo NBA functions", () => {
-  it("should find info nba logo for the Boston Celtics team", () => {
-    const logo = findLogo("BOS");
-    expect(logo).toHaveProperty("code");
-    expect(logo).toHaveProperty("logo");
-  });
-});
